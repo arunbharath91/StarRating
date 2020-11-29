80b5c54c0f3155f6eac502a7c238ed7061eca5a5
Arun Bharath <arunbharath91@gmail.com>
2020-11-29 09:02:27 +0530

    ++update

--- src/app/star-rating-config.ts
+++ src/app/star-rating-config.ts
@@ -1,3 +1,5 @@
 import { StarRating } from "./star-rating";
 
-const rating = new StarRating('#rating', {starcount: 9, readonly: true, value: 2.4});
+const rating = new StarRating('#rating', {starcount: 5, readonly: true, value: 2.4});
+
+const rating2 = new StarRating('#rating2', {starcount: 5, readonly: false, value: 3});
